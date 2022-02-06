5827c444b399336f60eebba1937e38c5f4e19a15
hendridg <hendridg@gmail.com>
2022-02-06 15:18:08 -0400

    Config babel, eslint, prettier and npm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, 'Luna'),
-    React.createElement('h3', {}, 'Dog'),
-    React.createElement('h3', {}, 'Havanese'),
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet.js";
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
   ]);
 };
 
-const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
-  ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
