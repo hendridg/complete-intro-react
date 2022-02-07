7c3b6a1f854a93850827ecfc6b0debf70dd6be5c
hendridg <hendridg@gmail.com>
2022-02-07 09:40:37 -0400

    Add Custom Hook useBreedList.js

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import useBreedList from "./useBreedList.js";
 import Pet from "./Pet.js";
 
 const ANIMAL = ["bird", "dog", "cat", "rabbit", "reptile"];
@@ -7,7 +8,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
