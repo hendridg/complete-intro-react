23577d84f1921dc84acae42a89e1a6bc5a87ae6a
hendridg <hendridg@gmail.com>
2022-02-10 10:01:18 -0400

    Manage State in a class Component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   state = { loading: true };
@@ -9,17 +10,22 @@ class Details extends Component {
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await response.json();
-    this.setState(Object.assign({ loading: false }, json.pets[0]));
+    // this.setState(Object.assign({ loading: false }, json.pets[0]));
+    this.setState({
+      loading: false,
+      ...json.pets[0],
+    });
   }
 
   render() {
-    const { name, animal, description, breed, city, state, loading } =
+    const { name, animal, description, breed, city, state, images, loading } =
       this.state;
     if (loading) {
       return <h2>Is Loading...</h2>;
     }
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
