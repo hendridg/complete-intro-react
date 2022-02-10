ad6fa93c7031cd8b18b6db0ed74191d64da1cc28
hendridg <hendridg@gmail.com>
2022-02-10 11:24:50 -0400

    Interactive Class Component

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,6 +5,14 @@ export default class Carousel extends Component {
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
+
+  //Hermosa forma de bind una funcion, con la funcion Arrow
+  handleIndexClick = (event) => {
+    // console.log(event.target.dataset.index);
+    this.setState({
+      active: event.target.dataset.index,
+    });
+  };
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -13,10 +21,11 @@ export default class Carousel extends Component {
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
-            // eslint-disable-next-line
             <img
               key={photo}
               src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
