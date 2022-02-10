6c11621b8d2075f2676228b4c094aa74e3943408
hendridg <hendridg@gmail.com>
2022-02-09 20:52:43 -0400

    Add class component Details

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,37 @@
-import { Link } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-const Details = () => {
-  return <h2>Hello!</h2>;
-};
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-export default Details;
+  async componentDidMount() {
+    const response = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await response.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    const { name, animal, description, breed, city, state, loading } =
+      this.state;
+    if (loading) {
+      return <h2>Is Loading...</h2>;
+    }
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
